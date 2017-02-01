e5d69f04f865d5ab5be9f88cab136b7b11894df3
Sheff3rd <thedoctorwhoovesmail@gmail.com>
2017-02-02 00:08:52 +0200

    react-router

---
+++ src/components/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Home Page </h1>
+        <p> Hello world! </p>
+      </div>
+    )
+  }
+}
+
+export default Home
